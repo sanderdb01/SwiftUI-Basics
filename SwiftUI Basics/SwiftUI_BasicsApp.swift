f4942e9f57747b043d0c29c757f7f7b9853dc902
David Sanders <sanderdb01@gmail.com>
2023-09-07 12:40:36 -0400

    Initial Commit

---
+++ SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by David Sanders on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
